061f283f81f8fd0671f76d085fb9d9ce58f58f9e
Frixuu <kontakt@lukasz.xyz>
2025-04-18 12:42:19 +0200

    Bump ABI

--- src/parser.c
+++ src/parser.c
@@ -14,7 +14,7 @@
 #pragma GCC optimize ("O0")
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 65
 #define LARGE_STATE_COUNT 15
 #define SYMBOL_COUNT 78
@@ -4359,7 +4359,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 63},
   [2] = {.lex_state = 63},
@@ -6312,6 +6312,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_hxml(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -6329,6 +6330,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_hxml(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
+    .name = "hxml",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 0,
+      .minor_version = 1,
+      .patch_version = 0,
+    },
   };
   return &language;
 }
